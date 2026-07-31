44d640cbc50d1c6f97a5be41654c4a189d0f768d
yazelin <yazelin@ching-tech.com>
2026-08-01 02:40:27 +0800

    publish: 章節 01,02 2026-08-01

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 轉生異世界的我 token 無限 — 離線快取(SHELL/ASSET 兩層;版本=內容雜湊,自動 bump)
-const SHELL = 'cs-shell-pr532c';
-const ASSET = 'cs-asset-6981a14de0';
+const SHELL = 'cs-shell-1a0pnma';
+const ASSET = 'cs-asset-c560913dc8';
 const SHELL_FILES = [
  "./",
  "./style.css",
